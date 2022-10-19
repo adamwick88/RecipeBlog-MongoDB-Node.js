7899fe52fac2b4df44c3dec6057b5c0c91c56b7b
Adam Wickstrom <adamwick1988@gmail.com>
2022-10-19 13:56:21 -0500

    first commit

--- app.js
+++ app.js
@@ -1,32 +1,60 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const fileUpload = require('express-fileupload');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
-
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const recipeRoutes= require("./routes/recipeRoutes")
 
-require('dotenv').config();
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
 
-app.use(express.urlencoded( { extended: true } ));
-app.use(express.static('public'));
-app.use(expressLayouts);
+// Passport config
+require("./config/passport")(passport);
 
-app.use(cookieParser('CookingBlogSecure'));
-app.use(session({
-  secret: 'CookingBlogSecretSession',
-  saveUninitialized: true,
-  resave: true
-}));
+//Connect To Database
+connectDB();
+
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+
+
+//Use flash messages for errors, info, ect...
 app.use(flash());
-app.use(fileUpload());
 
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+//Setup Routes For Which The Server Is Listening
+app.use("/", recipeRoutes);
 
-const routes = require('./server/routes/recipeRoutes.js')
-app.use('/', routes);
 
-app.listen(port, ()=> console.log(`Listening to port ${port}`));
+//Server Running
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
